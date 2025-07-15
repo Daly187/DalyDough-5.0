7cae2395d131877715d6914096c8523768b42e9d
Gregg Daly <Greggdaly187@gmail.com>
2025-07-15 22:14:55 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
-
 import 'dotenv/config';
-import type {NextConfig} from 'next';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
